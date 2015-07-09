43c959ea514da13f5ff0ba8158a4e7e7d5a79325
Ventskus Roman <scofield665@gmail.com>
2015-07-10 00:27:52 +0300

    Home workout order DESC

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @workouts = current_user.workouts.order("date ASC, created_at ASC")
+    @workouts = current_user.workouts.order("date DESC, created_at DESC")
     @max_results = WorkoutSet.joins(:workout).where("user_id", current_user.id).group("excercise_id").having("weight = MAX(weight)")
   end
 end
